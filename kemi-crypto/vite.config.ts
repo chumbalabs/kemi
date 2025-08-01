10cc052ac3f1d99d7565b1617de642776197abfa
chumbacash <amonchumba2018.com>
2025-08-01 09:13:18 +0300

    Fix frontend-backend API connection and environment variable loading

--- kemi-crypto/vite.config.ts
+++ kemi-crypto/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwind from '@tailwindcss/vite'
-import { join } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
